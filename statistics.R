a51aa276bcb1a69b5f2a83093e9905667830c8d9
Felipe Fronchetti <fronchettiemail@gmail.com>
2017-10-29 19:55:32 -0200

    Atualizando...

--- statistics.R
+++ statistics.R
@@ -2,11 +2,24 @@ library(ggplot2)
 
 data <- read.csv("/var/www/html/TCC-UTFPR/statistics.csv") # Lê o arquivo com as estatísticas
 
+dat = data[,c(2,3)]
+dat
 
-wssplot <- function(data, nc=15, seed=1234){
-wss <- (nrow(data)-1)*sum(apply(data,2,var))
-  for (i in 2:nc){
-    set.seed(seed)
-    wss[i] <- sum(kmeans(data, centers=i)$withinss)}
-  plot(1:nc, wss, type="b", xlab="Number of Clusters",
-       ylab="Within groups sum of squares")}
+mydata <- dat
+wss <- (nrow(mydata)-1)*sum(apply(mydata,2,var))
+for (i in 2:15) wss[i] <- sum(kmeans(mydata,
+                                     centers=i)$withinss)
+
+plot(1:15, wss, type="b", xlab="Number of Clusters",
+     ylab="Within groups sum of squares",
+     main="Assessing the Optimal Number of Clusters with the Elbow Method",
+     pch=20, cex=2)
+
+# Perform K-Means with the optimal number of clusters identified from the Elbow method
+set.seed(7)
+km2 = kmeans(dat, 3, nstart=100)
+
+# Examine the result of the clustering algorithm
+plot(dat, col =(km2$cluster +1) , main="K-Means result with 3 clusters", pch=20, cex=2)
+
+
