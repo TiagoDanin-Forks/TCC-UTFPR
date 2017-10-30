c4eb6f77eb5c2d0e01edd928a7ad4bfbfcae613b
Felipe Fronchetti <fronchettiemail@gmail.com>
2017-10-30 12:50:37 -0200

    Atualizando...

--- statistics.R
+++ statistics.R
@@ -1,25 +1,51 @@
-library(ggplot2)
+# Tutorial: http://www.sthda.com/english/articles/25-cluster-analysis-in-r-practical-guide/111-types-of-clustering-methods-overview-and-quick-start-r-code/
 
-data <- read.csv("/var/www/html/TCC-UTFPR/statistics.csv") # Lê o arquivo com as estatísticas
+install.packages("factoextra")
+install.packages("cluster")
+install.packages("magrittr")
+library("ggplot2")
+library("cluster")
+library("factoextra")
+library("magrittr")
+library(dplyr)
 
-dat = data[,c(2,3)]
-dat
+data_project <- read.csv("/var/www/html/TCC-UTFPR/projects_cluster_data.csv")
+# data_newcomers <- read.csv("/var/www/html/TCC-UTFPR/newcomers_cluster_data.csv")
 
-mydata <- dat
-wss <- (nrow(mydata)-1)*sum(apply(mydata,2,var))
-for (i in 2:15) wss[i] <- sum(kmeans(mydata,
-                                     centers=i)$withinss)
+values <- unique(data_project[,-1]) 
+row.names(values) <- unique(data_project[,1])
+values <- values %>%
+          na.omit() %>%          # Remove missing values (NA)
+          scale()                # Scale variables
 
-plot(1:15, wss, type="b", xlab="Number of Clusters",
-     ylab="Within groups sum of squares",
-     main="Assessing the Optimal Number of Clusters with the Elbow Method",
-     pch=20, cex=2)
+# fviz_nbclust(values, kmeans, method = "gap_stat") # Suggested number of cluster: 8
 
-# Perform K-Means with the optimal number of clusters identified from the Elbow method
-set.seed(7)
-km2 = kmeans(dat, 3, nstart=100)
+set.seed(123)
+km.res <- kmeans(values, 8, nstart = 25)
+# Visualize
+png(filename="/var/www/html/TCC-UTFPR/cluster.png")
+fviz_cluster(km.res, data = values,
+             ellipse.type = "convex",
+             palette = "jco",
+             ggtheme = theme_minimal())
+dev.off()
 
-# Examine the result of the clustering algorithm
-plot(dat, col =(km2$cluster +1) , main="K-Means result with 3 clusters", pch=20, cex=2)
+pam.res <- pam(values, 8)
+fviz_cluster(pam.res)
 
+# Compute hierarchical clustering
+res.hc <- data_project[,c(2:14)] %>%
+  scale() %>%                    # Scale the data
+  dist(method = "euclidean") %>% # Compute dissimilarity matrix
+  hclust(method = "ward.D2")     # Compute hierachical clustering
 
+png(filename="/var/www/html/TCC-UTFPR/cluster.png")
+# Visualize using factoextra
+# Cut in 4 groups and color by groups
+fviz_dend(res.hc, k = 4, # Cut in four groups
+          cex = 0.5, # label size
+          k_colors = c("#2E9FDF", "#00AFBB", "#E7B800", "#FC4E07"),
+          color_labels_by_k = TRUE, # color labels by groups
+          rect = TRUE # Add rectangle around groups
+)
+dev.off()
