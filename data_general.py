0bc6b0168ff9af34f12425b5b6548216a810a715
Felipe Fronchetti <fronchettiemail@gmail.com>
2017-11-18 17:30:11 -0200

    Updating...

---
+++ data_general.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+# Author:  Felipe Fronchetti
+# Contact: fronchettiemail@gmail.com
+# This code is responsible for generate data related to all the projects at once.
+
+try:
+    import json
+    import os
+    import csv
+    from datetime import datetime
+except ImportError as error:
+    raise ImportError(error)
+
+# Returns the file size in number of lines
+def get_file_size(file_name):
+    with open(file_name, 'r') as this:
+        for i, j in enumerate(this):
+            pass
+    return i + 1
+
+class GeneralData():
+    # Initial method. Sets values used during the entire process.
+    def __init__(self, dictionary):
+        self.about_list = []
+        self.dictionary = dictionary
+
+        for language in self.dictionary.keys():
+            repositories = self.dictionary[language]['items']
+            for repository in repositories:
+                self.about_list.append(repository)
+
+    def general(self):
+        projects = {}
+        for language in self.dictionary.keys():
+            repositories = self.dictionary[language]['items']
+            for repository in repositories:
+                folder = 'Dataset' + '/' + language + '/' + repository['name']
+
+                number_of_contributors = get_file_size(folder + '/first_contributions.txt')
+                number_of_commits = get_file_size(folder + '/contributions.txt')
+
+                with open(folder + '/pull_requests.json', 'r') as pulls_file:
+                    number_of_pulls = len(json.load(pulls_file))
+
+                projects[repository['name']] = {'Contributors': int(number_of_contributors), 'Contributions': int(number_of_commits), 'Pull-requests': int(number_of_pulls)}
+
+        fieldnames=['Project', 'Age', 'Stars', 'Watchers', 'Forks', 'Contributors', 'Contributions', 'Total_pulls', 'Has_issues', 'Has_project', 'Has_wiki']
+        general_file = open('data_general.csv', 'w')
+        writer = csv.DictWriter(general_file, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for project in self.about_list:
+            if project['has_issues'] is True:
+                has_issues = 1
+            else:
+                has_issues = 0
+            if project['has_projects'] is True:
+                has_projects =  1
+            else:
+                has_projects = 0
+            if project['has_wiki'] is True:
+                has_wiki = 1
+            else:
+                has_wiki = 0
+
+            age = 2017 - int(datetime.strptime(project['created_at'], '%Y-%m-%dT%H:%M:%SZ').date().year)
+            writer.writerow({'Project': project['name'],
+                             'Age': age,
+                             'Stars': project['stargazers_count'],
+                             'Watchers': project['watchers_count'],
+                             'Forks': project['forks_count'],
+                             'Has_issues': has_issues,
+                             'Has_project': has_projects,
+                             'Has_wiki': has_wiki,
+                             'Total_pulls': projects[project['name']]['Pull-requests'],
+                             'Contributors': projects[project['name']]['Contributors'],
+                             'Contributions': projects[project['name']]['Contributions']})
+
+if os.path.isfile('projects.json'):
+    with open('projects.json', 'r') as file:
+        projects_file = json.load(file)
+
+    G = GeneralData(projects_file)
+    G.general()
+else:
+    print('Error processing projects.json file.')
+    print('\033[97m\033[1m-> A file with a projects list does not exist. \033[0m Please, collect it first.')
---
+++ receptivity.R
@@ -0,0 +1,65 @@
+# Author: Felipe Fronchetti
+# Questions? fronchettiemail@gmail.com
+
+require("effsize")
+library(ggplot2)
+
+# Define your own working directory
+setwd("/var/www/html/TCC-UTFPR")
+
+# Reading files
+receptive <- read.csv("data_receptive.csv")
+nonreceptive <- read.csv("data_nonreceptive.csv")
+
+# Calculating MWW and Effect Size for receptive data
+# Newcomers x Open pull requests
+wilcox.test(receptive$newcomers_mean, receptive$pull_opened_mean)
+cliff.delta(receptive$newcomers_mean, receptive$pull_opened_mean)
+# Newcomers x Closed pull requests
+wilcox.test(receptive$newcomers_mean, receptive$pull_closed_mean)
+cliff.delta(receptive$newcomers_mean, receptive$pull_closed_mean)
+# Newcomers x Merged pull requests
+cliff.delta(receptive$newcomers_mean, receptive$pull_merged_mean)
+wilcox.test(receptive$newcomers_mean, receptive$pull_merged_mean)
+# Newcomers x Commits
+wilcox.test(receptive$newcomers_mean, receptive$commits_mean)
+cliff.delta(receptive$newcomers_mean, receptive$commits_mean)
+# Newcomers x Stars
+wilcox.test(receptive$newcomers_mean, receptive$stars_mean)
+cliff.delta(receptive$newcomers_mean, receptive$stars_mean)
+# Newcomers x Forks
+wilcox.test(receptive$newcomers_mean, receptive$forks_mean)
+cliff.delta(receptive$newcomers_mean, receptive$forks_mean)
+
+# Calculating MWW and Effect Size for nonreceptive data
+# Newcomers x Open pull requests
+wilcox.test(nonreceptive$newcomers_mean, nonreceptive$pull_opened_mean)
+cliff.delta(nonreceptive$newcomers_mean, nonreceptive$pull_opened_mean)
+# Newcomers x Closed pull requests
+wilcox.test(nonreceptive$newcomers_mean, nonreceptive$pull_closed_mean)
+cliff.delta(nonreceptive$newcomers_mean, nonreceptive$pull_closed_mean)
+# Newcomers x Merged pull requests
+wilcox.test(nonreceptive$newcomers_mean, nonreceptive$pull_merged_mean)
+cliff.delta(nonreceptive$newcomers_mean, nonreceptive$pull_merged_mean)
+# Newcomers x Commits
+wilcox.test(nonreceptive$newcomers_mean, nonreceptive$commits_mean)
+cliff.delta(nonreceptive$newcomers_mean, nonreceptive$commits_mean)
+# Newcomers x Stars
+wilcox.test(nonreceptive$newcomers_mean, nonreceptive$stars_mean)
+cliff.delta(nonreceptive$newcomers_mean, nonreceptive$stars_mean)
+# Newcomers x Forks
+wilcox.test(nonreceptive$newcomers_mean, nonreceptive$forks_mean)
+cliff.delta(nonreceptive$newcomers_mean, nonreceptive$forks_mean)
+
+summary(nonreceptive$newcomers_mean)
+summary(receptive$newcomers_mean)
+
+help("fisher.test")
+# Has readme / Hasn't readme (Receptive), Has Readme / Hasn't readme (Nonreceptive)
+readme.df = matrix(c(sum(receptive$has_readme == 1), sum(receptive$has_readme == 0), sum(nonreceptive$has_readme == 1), sum(nonreceptive$has_readme == 0)), nrow = 2, ncol=2)
+fisher.test(readme.df)
+
+# Scatterplots
+# Violin Plot: De um lado receptivos, do outro não receptivos, pra cada métrica
+plot(receptive$commits_mean, receptive$newcomers_mean, xlab="Requisições abertas por mês", ylab="Novatos distribuídos por semana")
+plot(nonreceptive$commits_mean, nonreceptive$newcomers_mean, xlab="Requisições abertas por mês", ylab="Novatos distribuídos por semana")
